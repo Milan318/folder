c54c1ecc1a451e96e8d5d9b6b5e1f917f2894ccf
Milan318 <158015797+Milan318@users.noreply.github.com>
2024-03-18 11:37:04 +0530

    Add files via upload

---
+++ Function/function addition.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int add(int,int);
+
+int main(void){
+  int result=0;
+
+  printf("%d",add(5,result));
+  return 0;
+}
+ int add(n,result){
+   if(n!=0){
+     result=result+n;
+     n--;
+     return add(n,result);
+   }
+   return result;
+ }
